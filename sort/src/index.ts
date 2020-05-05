6ca990229b0f41ab8916625599076316756019eb
Daniel Juventude <deljuven@ymail.com>
2020-05-04 21:58:04 -0300

    solution with BAD CODE

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,32 @@
-console.log(123);
+class Sorter {
+  constructor(public collection: number[] | string) {}
 
-const logSomething = () => {
-  console.log('something');
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logSomething();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // if collection is array of numbers
+        if (this.collection instanceof Array) {
+          /* instanceof is for the others values */
+          // collection === number[]
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+
+        // if collection is a string
+        if (typeof this.collection === 'string') {
+          /* typeof is for primitive values string, number and boolean */
+          // collection === string
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
