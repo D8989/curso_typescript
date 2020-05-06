b24ca2bf9e5b3a8fd0304eaed8f4671aa9ea8bcd
Daniel Juventude <deljuven@ymail.com>
2020-05-06 20:17:09 -0300

    solution with Not_The_Worst Code

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,32 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // if collection is array of numbers
-        if (this.collection instanceof Array) {
-          /* instanceof is for the others values */
-          // collection === number[]
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // if collection is a string
-        if (typeof this.collection === 'string') {
-          /* typeof is for primitive values string, number and boolean */
-          // collection === string
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
