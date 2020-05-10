cf0ebd47557e95c05bd59b7bb759b984c048b0ba
Daniel Juventude <deljuven@ymail.com>
2020-05-09 22:10:39 -0300

    solution with interfaces

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,20 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharaqctersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const characters = new CharaqctersCollection('Xaayb');
+const linkList = new LinkedList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+
+//const sorter = new Sorter(numbersCollection);
+// const sorter = new Sorter(characters);
+const sorter = new Sorter(linkList);
+
+linkList.print();
 sorter.sort();
-console.log(numbersCollection.data);
+linkList.print();
