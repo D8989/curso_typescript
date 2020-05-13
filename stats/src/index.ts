a7821b85ac37ec427734eb799fbc4667a1acf5ad
Daniel Juventude <deljuven@ymail.com>
2020-05-13 20:29:30 -0300

    create csvFileReader class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,10 +1,24 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('original.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('original.csv');
+reader.read();
 
-console.log(matches);
+console.log(reader.data);
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
