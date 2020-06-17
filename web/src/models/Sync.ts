5ba9575f17b80d309568dde662a067b0c82edff6
Daniel Juventude <deljuven@ymail.com>
2020-06-17 20:27:05 -0300

    advanced generic example

---
+++ web/src/models/Sync.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosResponse, AxiosPromise } from 'axios';
+
+export class Sync<T extends { id?: number }> {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
